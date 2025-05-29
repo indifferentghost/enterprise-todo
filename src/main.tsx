bf9305d49823743e6b11775c70e33bd0ab37261e
IndifferentGhost <12449668+indifferentghost@users.noreply.github.com>
2025-05-28 20:20:46 -0700

    Added and Setup React
    This includes adding the ancillary packages `react-dom` and `@vitejs/plugin-react` to connect with the browser DOM and plugin for Vite.

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = document.querySelector('#root');
+
+if (!root) {
+  throw new Error('Unable to find `root` node.');
+}
+
+createRoot(root).render(
+  <StrictMode>
+    Test
+  </StrictMode>,
+);
